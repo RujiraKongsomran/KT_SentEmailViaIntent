8d3c00e3b028da042cdd2d6de5cd14b116a7bf76
RujiraKongsomran <rujirabiwberry@gmail.com>
2021-07-13 15:09:50 +0700

    Finish

--- app/src/main/java/com/rujirakongsomran/kt_sentemailviaintent/MainActivity.kt
+++ app/src/main/java/com/rujirakongsomran/kt_sentemailviaintent/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.rujirakongsomran.kt_sentemailviaintent
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.rujirakongsomran.kt_sentemailviaintent.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +14,26 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        binding.btnSend.setOnClickListener {
+            val email = binding.edEmailAddress.text.toString()
+            val subject = binding.edSubject.text.toString()
+            val message = binding.edMessage.text.toString()
+
+            val addresses = email.split(",".toRegex()).toTypedArray()
+            composeEmail(addresses, subject, message)
+        }
+    }
+
+    private fun composeEmail(addresses: Array<String>, subject: String, message: String) {
+        val intent = Intent(Intent.ACTION_SENDTO).apply {
+            data = Uri.parse("mailto:")
+            putExtra(Intent.EXTRA_EMAIL, addresses)
+            putExtra(Intent.EXTRA_SUBJECT, subject)
+            putExtra(Intent.EXTRA_TEXT, message)
+        }
+        if (intent.resolveActivity(packageManager) != null) {
+            startActivity(intent)
+        }
     }
 }
